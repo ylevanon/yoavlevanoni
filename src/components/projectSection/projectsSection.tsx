34847501abc5191a8930384df2ac192d8ce1a3fa
ylevanon <yoavlevanoni@outlook.com>
2024-09-01 15:45:35 -0700

    added some content for optirungen.com

--- src/components/projectSection/projectsSection.tsx
+++ src/components/projectSection/projectsSection.tsx
@@ -5,7 +5,7 @@ const projects = [
   {
     name: "OptiRunGen.com",
     description:
-      "OptiRunGen is a linear programming application that generates globally optimal running routes based on user preferences. I developed a novel linear programming algorithm for this project and published and presented my findings at IEEE conference in Montreal Canada. The application is currently in development and will be released soon.",
+      "OptiRunGen is a linear programming application that generates globally optimal running routes based on user preferences. I developed a novel linear programming algorithm for this project and published and presented my findings at IEEE conference in Montreal Canada.",
     image: "/optirungen.png",
     github: "https://github.com/ylevanon/OptiRunRender",
     link: "https://www.optirungen.com/landing",
