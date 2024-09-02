b48c1f7f593b8070e5c50e6f35c9ecb2f74108ee
ylevanon <yoavlevanoni@outlook.com>
2024-09-02 12:55:02 -0700

    blog more mobile friendly

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -27,20 +27,24 @@ const components = {
 export default async function page({ params }: { params: { slug: string } }) {
   const data: BlogPost = await getData(params.slug);
   return (
-    <div className="mt-48 flex flex-col">
-      <h1 className="text-center font-bold text-5xl pb-8">
+    <div className="mt-16 md:mt-24 lg:mt-32 px-4  lg:px-6 max-w-4xl mx-auto">
+      <h1 className="text-center font-bold text-3xl md:text-4xl lg:text-5xl pb-4 md:pb-6">
         {data.title}
-        <hr className="w-64 h-1 mx-auto my-4 bg-teal-500 border-0 rounded m-b-19"></hr>
       </h1>
-      <Image
-        alt={data.title}
-        src={urlFor(data.titleImage).url()}
-        width={800}
-        height={800}
-        priority={true}
-        className={`rounded-lg mt-10 border self-center`}
-      />
-      <div className=" self-center mt-16 prose prose-xl dark:prose-invert prose-li:marker:bg-teal-500">
+      <hr className="w-32 md:w-48 lg:w-64 h-1 mx-auto my-4 bg-teal-500 border-0 rounded mb-6 md:mb-8" />
+
+      <div className="relative w-full aspect-w-16 aspect-h-9 mt-6 md:mt-8">
+        <Image
+          alt={data.title}
+          src={urlFor(data.titleImage).url()}
+          layout="fill"
+          objectFit="cover"
+          priority={true}
+          className="rounded-lg border"
+        />
+      </div>
+
+      <div className="mt-8 md:mt-12 prose prose-sm sm:prose-base lg:prose-lg dark:prose-invert prose-li:marker:bg-teal-500 max-w-none">
         <PortableText value={data.content} components={components} />
       </div>
     </div>
