afda7cb661baa4042f41e7d8d59ce6bd87fb0529
ylevanon <yoavlevanoni@outlook.com>
2024-09-23 16:24:05 -0700

    refereshing project page cache

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,8 @@
 import ProjectsSection from "@/components/projectSection";
 import React from "react";
 
+export const revalidate = 30;
+
 export default function Page() {
   return (
     <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 mt-10 pt-16 sm:py-32 md:pt-46 md:pb-48 md:flex-row md:space-x-4 md:text-left">
